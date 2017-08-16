770e0aa76dc3c3493a5b5e4356171de03f1b9679
Joy Lin <30854344+joylinmimi@users.noreply.github.com>
2017-08-16 11:24:29 +0800

    Add files via upload

---
+++ ft_strstr.c
@@ -0,0 +1,26 @@
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int j;
+	int size;
+
+	i = 0;
+	j = 0;
+	size = 0;
+	while (to_find[size])
+		size++;
+	if (size == 0)
+		return (str);
+	while (str[i])
+	{
+		while (to_find[j] == str[i + j])
+		{
+			if (j == size - 1)
+				return (str + i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
