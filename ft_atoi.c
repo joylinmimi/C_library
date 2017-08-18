ea3705bfa556a89d8fddae0dba46b6cabb788394
Joy Lin <30854344+joylinmimi@users.noreply.github.com>
2017-08-18 08:51:43 +0800

    8/18
    Computes the length of the string str up to but not including the terminating null character.

---
+++ ft_atoi.c
@@ -0,0 +1,27 @@
+int		ft_atoi(const char *str)
+{
+	int i;
+	int nbr;
+	int negative;
+
+	nbr = 0;
+	negative = 0;
+	i = 0;
+	while ((str[i] == '\n') || (str[i] == '\t') || (str[i] == '\v') ||
+			(str[i] == ' ') || (str[i] == '\f') || (str[i] == '\r'))
+		i++;
+	if (str[i] == '-')
+		negative = 1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] && (str[i] >= '0') && (str[i] <= '9'))
+	{
+		nbr *= 10;
+		nbr += (int)str[i] - '0';
+		i++;
+	}
+	if (negative == 1)
+		return (-nbr);
+	else
+		return (nbr);
+}
